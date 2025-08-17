1d2752ba1a3d9705ddce3968e449c6abc600445d
Roman Molotov <r.molotov@yahoo.com>
2025-08-17 20:01:57 +0400

    test extensions

---
+++ Packages/CustomExtensions/Tests/Editor/EditorTestsExtensions.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tests.Editor
+{
+    public static class EditorTestsExtensions
+    {
+        public static IEnumerable<string> GetAllScenesPaths() =>
+            AssetDatabase
+                .FindAssets("t:Scene", new[] { "Assets" })
+                .Select(AssetDatabase.GUIDToAssetPath);
+
+        public static bool HasMissingComponents(this GameObject gameObject) => 
+            GameObjectUtility.GetMonoBehavioursWithMissingScriptCount(gameObject) > 0;
+    }
+}
